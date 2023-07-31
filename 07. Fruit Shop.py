96461b749ee137fbb555cef6879d053f7e012135
me-madness <58656549+me-madness@users.noreply.github.com>
2023-07-31 23:19:04 +0200

    seventh it's finish

---
+++ 07. Fruit Shop.py
@@ -0,0 +1,40 @@
+product = input().lower()
+day = input().lower()
+quantity = float(input())
+result = 0
+
+if day == 'saturday' or day == 'sunday':
+    if product == 'banana':
+        result = quantity * 2.70
+    elif product == 'apple':    
+        result = quantity * 1.25
+    elif product == 'orange':    
+        result = quantity * 0.90
+    elif product == 'grapefruit':    
+        result = quantity * 1.60
+    elif product == 'kiwi':    
+        result = quantity * 3.00
+    elif product == 'pineapple':    
+        result = quantity * 5.60
+    elif product == 'grapes':    
+        result = quantity * 4.20
+    else :    
+        print('error')
+else:
+    if product == 'banana':
+        result = quantity * 2.50
+    elif product == 'apple':    
+        result = quantity * 1.20
+    elif product == 'orange':    
+        result = quantity * 0.85
+    elif product == 'grapefruit':    
+        result = quantity * 1.45
+    elif product == 'kiwi':    
+        result = quantity * 2.70
+    elif product == 'pineapple':    
+        result = quantity * 5.50
+    elif product == 'grapes':    
+        result = quantity * 3.85
+    else :    
+        print('error')  
+print(round(result, 2))      
