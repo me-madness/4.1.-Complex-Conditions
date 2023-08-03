38190db2f406dcc7cb4db63700e1be2909756b79
me_madness <p_rangel@abv.bg>
2023-08-03 14:45:00 +0200

    13 it's not finish at all

---
+++ 13. Point in the Figure.py
@@ -0,0 +1,18 @@
+h = int(input())
+x = int(input())
+y = int(input())
+
+out_rectangle1 = 
+out_rectangle2 = 
+
+in_rectangle1 = 
+in_rectangle2 = 
+
+border = 
+
+if out_rectangle1 and out_rectangle2:
+    print('outside')
+elif in_rectangle1 or in_rectangle2:
+    print('inside')   
+else:
+    print('border')    
